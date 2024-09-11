9512e35b8ad25ab685e34c8c77fd8b9d28bc01e5
CalebMohamed <calebnoahmohamed@gmail.com>
2024-09-11 17:39:44 +0100

    chapter 08 exercises complete

--- LearnYouAHaskell/00-exercises/08-making-our-own-types-and-typeclasses.hs
+++ LearnYouAHaskell/00-exercises/08-making-our-own-types-and-typeclasses.hs
@@ -12,13 +12,24 @@
  - Uncomment the following declarations to complete the implementation, and provide an implementation for instance Show Card
  -}
 
---data Suit = 
---data Digit = 
---data Card = 
+data Suit = Clubs | Diamonds | Hearts | Spades deriving (Show, Eq)
+data Digit = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Eq, Ord, Show)
+data Card = Card {digit :: Digit, suit :: Suit } deriving (Eq)
+
+instance Ord Card where
+    (Card d1 s1) >= (Card d2 s2) = d1 >= d2
+    (Card d1 s1) > (Card d2 s2) = d1 > d2
+    (Card d1 s1) <= (Card d2 s2) = d1 <= d2
+    (Card d1 s1) < (Card d2 s2) = d1 < d2
+
+instance Show Card where
+    show (Card d s) = "The " ++ show d ++ " of " ++ show s
 
 -- We should be able to provide a function which returns the higher ranked card:
 betterCard :: Card -> Card -> Card
-betterCard x y = undefined
+betterCard x y
+    | x < y     = y
+    | otherwise = x
 
 -- Here is a new Typeclass, which represents some kind of playing hand in a game.
 -- It returns True for a "winning hand", depending on the rules for the type of class we are playing with
@@ -27,13 +38,18 @@ class Hand a where
 
 -- Implement Hand for Card, where play returns true if the list contains the Ace of Spades
 instance Hand Card where
-    play c = undefined
+    play = foldr (\x acc -> x == Card Ace Spades || acc) False
 
 -- Create a new Coin type
---data Coin = 
+data Coin = Tails | Heads deriving (Eq, Ord, Bounded, Show)
 
 -- Implement Hand for Coin, where play returns true if there are ten heads in a row in the list
 instance Hand Coin where
-	play c =  undefined
+    play :: [Coin] -> Bool
+    play c = foldr p 0 c == 10
+        where p c acc
+                | acc >= 10 = acc
+                | c == Heads = acc + 1
+                | otherwise = 0
 
 -- Have a play with implementing Hand for some other types, for instance Int and Bool
